de1bc529c50a49d058b69b1776ba027166a7792f
florian bricchi <lokin84250@live.fr>
2024-03-14 09:46:12 +0100

    Fix path utils

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -1,8 +1,8 @@
-import * as React from "react"
 import * as AccordionPrimitive from "@radix-ui/react-accordion"
 import { ChevronDownIcon } from "@radix-ui/react-icons"
+import * as React from "react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/src/lib/utils"
 
 const Accordion = AccordionPrimitive.Root
 
@@ -52,4 +52,5 @@ const AccordionContent = React.forwardRef<
 ))
 AccordionContent.displayName = AccordionPrimitive.Content.displayName
 
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
+export { Accordion, AccordionContent, AccordionItem, AccordionTrigger }
+
